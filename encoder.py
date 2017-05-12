7e1d4d690cd6489ca43ba1ba248e868f0fe4fbe1
kuangliu <kuang.liu@hotmail.com>
2017-05-12 13:25:04 +0800

    Add nms

--- encoder.py
+++ encoder.py
@@ -112,6 +112,59 @@ class DataEncoder:
         conf[iou<threshold] = 0       # background
         return loc, conf
 
+    def nms(self, bboxes, scores, threshold=0.5, mode='union'):
+        '''Non maximum suppression.
+
+        Args:
+          bboxes: (tensor) bounding boxes, sized [N,4].
+          scores: (tensor) bbox scores, sized [N,].
+          threshold: (float) overlap threshold.
+          mode: (str) 'union' or 'min'.
+
+        Returns:
+          keep: (tensor) selected indices.
+
+        Ref:
+          https://github.com/rbgirshick/py-faster-rcnn/blob/master/lib/nms/py_cpu_nms.py
+        '''
+        x1 = bboxes[:,0]
+        y1 = bboxes[:,1]
+        x2 = bboxes[:,2]
+        y2 = bboxes[:,3]
+
+        areas = (x2-x1) * (y2-y1)
+        _, order = scores.sort(0, descending=True)
+
+        keep = []
+        while order.numel() > 0:
+            i = order[0]
+            keep.append(i)
+
+            if order.numel() == 1:
+                break
+
+            xx1 = x1[order[1:]].clamp(min=x1[i])
+            yy1 = y1[order[1:]].clamp(min=y1[i])
+            xx2 = x2[order[1:]].clamp(max=x2[i])
+            yy2 = y2[order[1:]].clamp(max=y2[i])
+
+            w = (xx2-xx1).clamp(min=0)
+            h = (yy2-yy1).clamp(min=0)
+            inter = w*h
+
+            if mode == 'union':
+                ovr = inter / (areas[i] + areas[order[1:]] - inter)
+            elif mode == 'min':
+                ovr = inter / areas[order[1:]].clamp(max=areas[i])
+            else:
+                raise TypeError('Unknown nms mode: %s.' % mode)
+
+            ids = (ovr<=threshold).nonzero().squeeze()
+            if ids.numel() == 0:
+                break
+            order = order[ids+1]
+        return torch.LongTensor(keep)
+
     def decode(self, loc, conf):
         '''Transform predicted loc/conf back to real bbox locations and class labels.
 
@@ -121,13 +174,15 @@ class DataEncoder:
 
         Returns:
           boxes: (tensor) bbox locations, sized [#obj, 4].
-          labels: (tensor) class labels, sized [#obj,1]
+          labels: (tensor) class labels, sized [#obj,1].
         '''
         variances = [0.1, 0.2]
         wh = torch.exp(loc[:,2:]*variances[1]) * self.default_boxes[:,2:]
         cxcy = loc[:,:2] * variances[0] * self.default_boxes[:,2:] + self.default_boxes[:,:2]
         boxes = torch.cat([cxcy-wh/2, cxcy+wh/2], 1)  # [8732,4]
 
-        _, labels = conf.max(1)  # [8732,1]
+        max_conf, labels = conf.max(1)  # [8732,1]
         ids = labels.squeeze(1).nonzero().squeeze(1)  # [#boxes,]
-        return boxes[ids], labels[ids]
+
+        keep = self.nms(boxes[ids], max_conf[ids].squeeze(1))
+        return boxes[ids][keep], labels[ids][keep], max_conf[ids][keep]
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 
 from torch.autograd import Variable
@@ -15,7 +16,7 @@ net.load_state_dict(torch.load('model/net.pth'))
 net.eval()
 
 # Load test image
-img = Image.open('./img/img2.jpg')
+img = Image.open('./image/img1.jpg')
 img = img.resize((300,300))
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
@@ -26,11 +27,10 @@ loc, conf = net(Variable(inputs[None,:,:,:], volatile=True))
 
 # Decode
 data_encoder = DataEncoder()
-boxes, labels = data_encoder.decode(loc.data.squeeze(0), conf.data.squeeze(0))
+boxes, labels, scores = data_encoder.decode(loc.data.squeeze(0), F.softmax(conf.squeeze(0)).data)
 
 draw = ImageDraw.Draw(img)
 for box in boxes:
     box *= 300
-    draw.rectangle(list(box))
-
+    draw.rectangle(list(box), outline='red')
 img.show()
